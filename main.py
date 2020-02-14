6ca2e48e946d765a1b68569ed2a2526f04a861c9
Itay Gabbay <itaygabb@gmail.com>
2020-02-14 12:03:55 +0200

    Adding code

--- main.py
+++ main.py
@@ -23,28 +23,5 @@ def main():
     print("Landmark features:", evaluate(landmark, rankings, 9))
     print("Landmark + Distance features:", evaluate(landmark_distance, rankings))
 
-    X = list(range(1,11))
-    ydist = []
-    ylmrk = []
-    for k in range(1,11):
-        ydist.append(evaluate(distance, rankings, k)[0][0])
-        ylmrk.append(evaluate(landmark, rankings, k)[0][0])
-
-    plt.plot(X, ydist)
-    plt.plot(X, ylmrk)
-    plt.show()
-    metrics = [("BP", pd.read_csv('./result_BP.csv')),
-                ("CH", pd.read_csv('./result_CH.csv')),
-                ("DB", pd.read_csv('./result_DB.csv')),
-                ("DU", pd.read_csv('./result_DU.csv')),
-                ("HKK", pd.read_csv('./result_HKK.csv')),
-                ("HL", pd.read_csv('./result_HL.csv')),
-                ("MC", pd.read_csv('./result_MC.csv')),
-                ("SIL", pd.read_csv('./result_SIL.csv'))]
-    
-    # for name, rk in metrics:
-    #     print("Distance", name, ":", evaluate(distance, rk))
-    #     print("Landmark", name, ":", evaluate(landmark, rk))
-
 if __name__ == "__main__":
     main()
